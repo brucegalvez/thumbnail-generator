1cf2534188dc1432b6591c46ebf801efd259c9a1
Rodrigo Bruce Galvez <rodrigobrucegalvez@gmail.com>
2021-01-27 09:55:40 -0500

    chore: :rocket: Test deploy

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 exports.handler = async (event) => {
-    // const name = event['name'];
-    
-    const body = JSON.parse(event.body)
-    
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(`Hello, heyy ${body.name}!`)
-    };
-    
-    return response;
+  // const name = event['name'];
+
+  const body = JSON.parse(event.body);
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(`Hola! ${body.name}!`),
+  };
+
+  return response;
 };
